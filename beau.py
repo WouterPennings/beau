6d62d4dc873db8a4de0ef461a7b08bca33da0e64
WouterPennings <wouterpennings@gmail.com>
2022-02-11 14:57:40 +0100

    feat: if-statement hard-coded

--- beau.py
+++ beau.py
@@ -2,12 +2,12 @@ import sys
 from dataclasses import dataclass
 from enum import Enum, auto
 
-CUSTOM_TAGS = ["let"]
-
 class TokenType(Enum):
     Native = auto()             # HTML Tokens: h1, p, iframe, ul
     Value = auto()              # Just a piece of text
     Let = auto()                # Custom Token: declaring variable
+    If = auto()
+    Else = auto()
 
 @dataclass          
 class Attr:            
@@ -33,11 +33,13 @@ class Let:                      # For creating and reading a variable, depends o
 @dataclass
 class If:                       # If statement 
     Literal: str        
+    attr_pos: bool
     Attributes: list[Attr] 
 
 @dataclass
 class Else:                     # Else block for if-statement
-    Literal: str        
+    Literal: str  
+    attr_pos: bool      
     Attributes: list[Attr] 
 
 @dataclass
@@ -48,47 +50,94 @@ class Token:
 class Compiler:
     def __init__(self, tokens: list[Token]):
         self.tokens: list[Token] = tokens
+        self.current: int = 0
+        self.variables: dict
+
+    def next_token(self):
+        self.current += 1
+
+    def current_token(self):
+        return self.tokens[self.current]
 
     def compiler_to_html(self):
-        variables = {}
         html = ""
-        for token in self.tokens:
-            match token.Type:
-                case TokenType.Native:
-                    if not token.Tag.attr_pos:
-                        html += "</{}>".format(token.Tag.Literal)
+        while self.current < len(self.tokens):
+            html += self.get_html_token()
+            self.next_token()
+        return html
+
+    def get_html_token(self):
+        html = ""
+        match self.current_token().Type:
+            case TokenType.Native:
+                if not self.current_token().Tag.attr_pos:
+                    html += "</{}>".format(self.current_token().Tag.Literal)
+                else:
+                    attr = " "
+                    for a in self.current_token().Tag.Attributes:
+                        attr += a.Name + a.Op + a.Value + " "
+                    html += "<{} {}>".format(self.current_token().Tag.Literal, attr)
+            case TokenType.Value:
+                html += self.current_token().Tag.Value
+            case TokenType.Let:
+                attributes = self.current_token().Tag.Attributes
+                allowed_attributes = ["name", "value"]
+                for a in attributes:
+                    if not a.Name in allowed_attributes:
+                        self.throw_error("The attribute with the name: {}, is not known in this context".format(a.Name))
+                if len(self.current_token().Tag.Attributes) == 2:
+                    if attributes[0].Value in self.variables:
+                        print("[ BEAU WARNING] Variable with the same name has already been created")
+                    # if not attributes[0].Value.isupper():
+                    #     self.throw_error("The variable name: {}, should be all uppercase".format(attributes[0].Value))
+                    self.variables[attributes[0].Value] = attributes[1].Value[1:-1]
+                elif len(attributes) == 1:
+                    if not attributes[0].Name == "name" and attributes[0].Value == "": 
+                        self.throw_error("You have a attribute without a name")
+                    if attributes[0].Value == "":
+                        self.throw_error("You called a variable with a name without any characters")
+                    elif not attributes[0].Value in self.variables:
+                        self.throw_error("You called '{}', but that does not exist".format(attributes[0].Value[1:-1]))
                     else:
-                        attr = " "
-                        for a in token.Tag.Attributes:
-                            attr += a.Name + a.Op + a.Value + " "
-                        html += "<{} {}>".format(token.Tag.Literal, attr)
-                case TokenType.Value:
-                    html += token.Tag.Value
-                case TokenType.Let:
-                    attributes = token.Tag.Attributes
-                    allowed_attributes = ["name", "value"]
+                        html += self.variables[attributes[0].Value]
+                else:
+                    self.throw_error("You have supplied: {} attributes, but '{}' can only take a max of 2".format(len(attributes), self.current_token().Tag.Literal))
+            case TokenType.If:
+                if self.current_token().Tag.attr_pos:
+                    attributes = self.current_token().Tag.Attributes
+                    allowed_attributes = ["left", "right", "op"]
                     for a in attributes:
                         if not a.Name in allowed_attributes:
                             self.throw_error("The attribute with the name: {}, is not known in this context".format(a.Name))
-                    if len(token.Tag.Attributes) == 2:
-                        if attributes[0].Value in variables:
-                            print("[ BEAU WARNING] Variable with the same name has already been created")
-                        # if not attributes[0].Value.isupper():
-                        #     self.throw_error("The variable name: {}, should be all uppercase".format(attributes[0].Value))
-                        variables[attributes[0].Value] = attributes[1].Value[1:-1]
-                    elif len(attributes) == 1:
-                        if not attributes[0].Name == "name" and attributes[0].Value == "": 
-                            self.throw_error("You have a attribute without a name")
-                        if attributes[0].Value == "":
-                            self.throw_error("You called a variable with a name without any characters")
-                        elif not attributes[0].Value in variables:
-                            self.throw_error("You called '{}', but that does not exist".format(attributes[0].Value[1:-1]))
-                        else:
-                            html += variables[attributes[0].Value]
+                    if not self.eval_comparison(int(self.current_token().Tag.Attributes[0].Value[1:-1]), self.current_token().Tag.Attributes[1].Value[1:-1], int(self.current_token().Tag.Attributes[2].Value[1:-1])):
+                        while True:
+                            self.next_token()
+                            if self.current_token().Type == TokenType.If:
+                                if not self.current_token().Tag.attr_pos:
+                                    self.current -= 1
+                                    break
                     else:
-                        self.throw_error("You have supplied: {} attributes, but '{}' can only take a max of 2".format(len(attributes), token.Tag.Literal))
+                        while True:
+                            self.next_token()
+                            if self.current_token().Type == TokenType.If:
+                                if not self.current_token().Tag.attr_pos:
+                                    self.current -= 1
+                                    break
+                            html += self.get_html_token()    
         return html
-
+        
+    def eval_comparison(self, left: int, op: str, right: int):
+        print(left, op, right)
+        match op:
+            case 'equals':
+                return (left == right)
+            case 'greater':
+                return (left > right)
+            case 'smaller':
+                return (left < right)
+            case _:
+                self.throw_error("Operator chosen for comparison is not implemented")
+    
     def throw_error(self, text):
         print("[ BEAU ERROR] {}".format(text))
         quit() 
@@ -127,19 +176,23 @@ class Parser:
                         syntax_tokens.append(Token(TokenType.Value, Value(current_literal)))
                     elif self.current_char == '/':                                                  
                         current_literal = self.get_until(['>'])
-                        syntax_tokens.append(Token(TokenType.Native, Native(current_literal[0:], False, [])))
+                        match current_literal:
+                            case "if": syntax_tokens.append(Token(TokenType.If, If(tag, False, [])))
+                            case "else": syntax_tokens.append(Token(TokenType.Else, Else(tag, False, [])))
+                            case _: syntax_tokens.append(Token(TokenType.Native, Native(current_literal[0:], False, [])))
                     else:                                                                            
                         current_literal = self.current_char + self.get_until(['/', '>']) 
                         tag, index = self.get_tag(current_literal)
                         attributes = self.get_attributes(current_literal[index:].strip())
-                        if tag in CUSTOM_TAGS:
-                            syntax_tokens.append(Token(TokenType.Let, Let(tag, attributes)))
-                        else:
-                            syntax_tokens.append(Token(TokenType.Native, Native(tag, True, attributes)))
+                        match tag:
+                            case "let": syntax_tokens.append(Token(TokenType.Let, Let(tag, attributes)))
+                            case "if": syntax_tokens.append(Token(TokenType.If, If(tag, True, attributes)))
+                            case "else": syntax_tokens.append(Token(TokenType.Else, Else(tag, True, attributes)))
+                            case _ : syntax_tokens.append(Token(TokenType.Native, Native(tag, True, attributes)))
                 case _:
                     text = self.input[self.index - 1] + self.current_char + self.get_until(['<'])
                     syntax_tokens.append(Token(TokenType.Value, Value(text)))  
-                    self.prev_character()  
+                    self.prev_character()   
                     self.prev_character()      
             self.next_character() 
         return syntax_tokens
